e3845bcdb0f9d514ed0804b3718d4436ff0af2a6
ampunkboyx <afif.mynet@gmail.com>
2024-04-18 03:39:04 +0800

    create lobby and room

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,17 @@
 # FYP/autotranslator/urls.py
 from django.urls import path, include
 from .views import authView, home
+from . import views
 
 urlpatterns = [
     path("", home, name="home"),
     path("signup/", authView, name="authView"),
     path("accounts/", include("django.contrib.auth.urls")),
+    path('', views.lobby),
+    path('room/', views.room),
+    path('get_token/', views.getToken),
+
+    path('create_member/', views.createMember),
+    path('get_member/', views.getMember),
+    path('delete_member/', views.deleteMember),
 ]
