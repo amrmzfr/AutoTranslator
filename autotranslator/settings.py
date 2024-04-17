fd109a3fefa1dbd441225aacaf6fe7873abd2f72
ampunkboyx <afif.mynet@gmail.com>
2024-04-18 03:55:20 +0800

    fix static files

--- autotranslator/settings.py
+++ autotranslator/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
