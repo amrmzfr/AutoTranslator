927f7b80fa892e313e57b3ad97a8e50f797fc3d2
ampunkboyx <afif.mynet@gmail.com>
2024-04-22 14:36:52 +0800

    App directory

--- autotranslator/settings.py
+++ autotranslator/settings.py
@@ -58,7 +58,8 @@ ROOT_URLCONF = 'autotranslator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [],
+        # 'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
